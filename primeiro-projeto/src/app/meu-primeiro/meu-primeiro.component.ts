9bebd786f452d7c161855c62ff34eceaa8dcfa8e
sinvalfelisberto <sinvalfelisberto@gmail.com>
2022-08-14 06:52:50 -0300

    Estudo sobre services

--- primeiro-projeto/src/app/meu-primeiro/meu-primeiro.component.ts
+++ primeiro-projeto/src/app/meu-primeiro/meu-primeiro.component.ts
@@ -3,7 +3,7 @@ import { Component } from "@angular/core";
 @Component({
   selector: 'meu-primeiro-component',
   template: `
-            <p>Meu primeiro component com Angular2!</p>
+            <p>Meu primeiro component com Angular2 -->!</p>
             `
 })
 export class MeuPrimeiroComponent { }

517f6c9deb622b00c2af1eed9c06eab971ec3b1e
sinvalfelisberto <sinvalfelisberto@gmail.com>
2022-08-28 18:33:47 -0300

    Aprendendo sobre data binding

--- primeiro-projeto/src/app/data-binding/data-binding.component.ts
+++ primeiro-projeto/src/app/data-binding/data-binding.component.ts
@@ -9,15 +9,32 @@ export class DataBindingComponent implements OnInit {
 
   url: string = 'httl://www.felisberto.com.br';
   urlImagem: string = 'https://i.picsum.photos/id/533/200/200.jpg?hmac=HvhCl1BSaQrsbedBJm-X8gfnZGp_222QGZ-mYnstPiA';
-
   cursoAngular: boolean = true;
-
+  valorAtual: string = "";
+  valorSalvo: string = "";
+  isMouseOver: boolean = false;
   getValor(){
     return 1;
   }
 
   getCurtirCurso(){
     return true;
+  }
+
+  botaoClicado(){
+    alert(this.valorAtual);
+  }
+
+  onKeyUp(event: KeyboardEvent){
+    this.valorAtual = ((<HTMLInputElement>event.target).value);
+  }
+
+  salvarValor(valor: any){
+    this.valorSalvo = valor;
+  }
+
+  onMouseOverOut(){
+    this.isMouseOver = !this.isMouseOver;
   }
 
   constructor() { }
